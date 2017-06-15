a9f5ba421b6242ff2003fe0cb385cbedafa5134b
Your Name <forever.alone92@gmail.com>
2017-06-15 10:44:34 +0700

    Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,32 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  attr_accessor :login
   has_one :bank_account
   has_one :cart
   has_many :comments
   has_many :orders
   has_many :reviews
   has_many :financial_reports
+  validates :username, presence: true, uniqueness: {case_sensitive: false}
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
+  validate :validate_username
+
+  class << self
+    def find_for_database_authentication warden_conditions
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value",
+          {:value => login.downcase}]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_h).first
+      end
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add :username, :invalid
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "sessions"
+  }
   devise_scope :user do
     get "/register", to: "devise/registrations#new", as: :register
-    get "/signin", to: "devise/sessions#new", as: :signin
+    get "/signin", to: "sessions#new", as: :signin
   end
   root to: "static_pages#home"
 end
